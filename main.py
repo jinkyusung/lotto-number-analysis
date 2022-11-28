3b11b1a691b375f14c99095a50513ef3941d8fd6
Jinkyu Sung <sungjk1999@gmail.com>
2022-11-28 21:34:51 +0900

    Create main.py - Mean, Variance를 구하고 DataFrame으로 리턴하는 작업

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import lotto
 import pandas as pd
-import os
 
 
-def onehot(data: list[list]):
+def onehot(data: list[list]) -> list:
     def encode(nums: list):
         dummy = [0] * 45
         for n in nums:
@@ -12,14 +11,23 @@ def onehot(data: list[list]):
     return list(map(encode, data))
 
 
-def make_dataframe(index, data):
+def lotto_df(index: list, data: list[list]):
     df = pd.DataFrame(data=onehot(data), index=index, columns=list(range(1, 46)))
     return df
+
+
+def mean_var_df(lotto_df: pd.DataFrame) -> pd.DataFrame:
+    df = pd.DataFrame()
+    df['Mean'] = lotto_df.mean()
+    df['Variance'] = lotto_df.var()
+    return df.T
 
 
 if __name__ == '__main__':
     url = "https://dhlottery.co.kr/common.do?method=main"
     lotto_dates, lotto_nums = lotto.get_every_lotto(url)
-    lotto_df = make_dataframe(lotto_dates, lotto_nums)
-    path = os.getcwd().replace('\\','/') + '/lotto_df.csv'
-    lotto_df.to_csv(path)
+    result1 = lotto_df(lotto_dates, lotto_nums)
+    result2 = mean_var_df(result1)
+
+    print(result1)
+    print(result2)
